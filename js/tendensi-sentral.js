ed506c87f8c56661c05b9c121eefeda5587c9a86
fanuhi <fadhlinurhimawan@gmail.com>
2023-04-10 13:16:44 +0700

    add comment to median function

--- js/tendensi-sentral.js
+++ js/tendensi-sentral.js
@@ -3,24 +3,27 @@ var data;
 
 function median(){
     var data = document.getElementById("input-data").value;
+
+    // mengubah text ke array dengan delimiter ,
     var data = data.split(",");
+
+    // mengurutkan data
     var sortedLst = data.sort(function(a, b){return a-b});
 
+    // mendapatkan panjang array
     let listLen = data.length;
     
+    // pembagian pembulatan kebawah
     let index = Math.floor((listLen - 1) / 2 );
 
 
     if (listLen % 2){
+        // apabila panjang array dimodulus 2, maka nilai tengah langsung dapat diketahui, atau median terdapat dalam value dalam array tersebut
         alert( sortedLst[index]  ) 
     } else {
+        // mendapatkan batas atas dan bawah dari nilai tengah, kemudian dijumlahkan lalu dibagi 2
         var result = ( ( parseInt(sortedLst[index])  + parseInt(sortedLst[index + 1])) /2 ) 
         document.getElementById("result").innerHTML = result;
     }
 
-}
-
-
-function convert_to_list(data) {
-    var data = data.split(",");
 }
